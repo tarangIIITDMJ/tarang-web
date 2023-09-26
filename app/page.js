889785dae8040ccad00da47d08be9da3842d8ef0
Om Thakare <omthakare1606@gmail.com>
2023-09-27 03:19:33 +0530

    fix: Gallery section import in page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import EventSection from "./components/HomePageSections/EventSection";
 import HeroMain from "./components/HomePageSections/HeroMain";
 import SponsorSection from "./components/HomePageSections/SponsorSection";
 import SubMainSection from "./components/HomePageSections/SubMainSection";
+import GalleryView from "./components/HomePageSections/GalleryView";
 
 export default function Home() {
   return (
