a5b92b730ee3777fec8d6aa50d9f01b01d04c89d
Prajjwal kapoor <prajjwal.kapoor.mail@gmail.com>
2023-09-26 22:23:05 +0530

    fix: hero responsive

--- app/components/HomePageSections/HeroMain.jsx
+++ app/components/HomePageSections/HeroMain.jsx
@@ -1,49 +1,83 @@
 "use client";
-import { Button, Text, Container, Image } from "@mantine/core";
+import { Button, Text, Container, Image, Box } from "@mantine/core";
 import { IconArrowRight } from "@tabler/icons-react";
 import { useMediaQuery } from "@mantine/hooks";
 
 const HeroMain = () => {
   const isMobileView = useMediaQuery("(max-width: 768px)");
   return (
-    <Container
-      h="100%"
-      p={0}
-      m={0}
-      miw="100%"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        pointerEvents: "auto",
-      }}
-    >
-      <Image
-        src={"/tarangHeading.svg"}
-        alt="Tarang Heading"
-        w={isMobileView ? 300 : 675}
-      />
-      <Text
-        size={isMobileView ? "20px" : "28px"}
-        c="white"
-        mt={22}
-        ta={"center"}
-        px={60}
-      >
-        One line about Tarang 2023 cultural fest
-      </Text>
+    <Container h="100vh" m={0} bg="blue" miw="100%" style={styles.container}>
+      <video loop muted autoPlay style={styles.video}>
+        <source
+          src="https://res.cloudinary.com/prajjwalcdn/video/upload/v1695590772/video_seh4ks.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <Box style={styles.videoOverlay}>
+        <Container
+          h="100%"
+          p={0}
+          m={0}
+          miw="100%"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            pointerEvents: "auto",
+          }}
+        >
+          <Image
+            src={"/tarangHeading.svg"}
+            alt="Tarang Heading"
+            w={isMobileView ? 300 : 675}
+          />
+          <Text
+            size={isMobileView ? "20px" : "28px"}
+            c="white"
+            mt={22}
+            ta={"center"}
+            px={60}
+          >
+            One line about Tarang 2023 cultural fest
+          </Text>
 
-      <Button
-        color="black"
-        size={isMobileView ? "md" : "lg"}
-        mt={isMobileView ? 20 : 44}
-        rightSection={<IconArrowRight />}
-      >
-        Explore Now
-      </Button>
+          <Button
+            color="black"
+            size={isMobileView ? "md" : "lg"}
+            mt={isMobileView ? 20 : 44}
+            rightSection={<IconArrowRight />}
+          >
+            Explore Now
+          </Button>
+        </Container>
+      </Box>
     </Container>
   );
+};
+
+const styles = {
+  container: {
+    position: "relative",
+    overflow: "hidden", // Ensure video doesn't overflow the container
+  },
+  video: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+  },
+  videoOverlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    minWidth: "100%",
+    height: "100vh",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    pointerEvents: "none",
+  },
 };
 
 export default HeroMain;
--- app/page.js
+++ app/page.js
@@ -14,17 +14,7 @@ import HeroMain from "./components/HomePageSections/HeroMain";
 export default function Home() {
   return (
     <MainAppShell>
-      <Container h="100vh" m={0} bg="blue" miw="100%" style={styles.container}>
-        <video loop muted autoPlay style={styles.video}>
-          <source
-            src="https://res.cloudinary.com/prajjwalcdn/video/upload/v1695590772/video_seh4ks.mp4"
-            type="video/mp4"
-          />
-        </video>
-        <Box style={styles.videoOverlay}>
-          <HeroMain />
-        </Box>
-      </Container>
+      <HeroMain />
       <Box style={styles.sponsorUs}>
         <Stack align="center" gap="2rem">
           <Text size="3rem" c="#F2F2F2">
@@ -45,27 +35,6 @@ export default function Home() {
 }
 
 const styles = {
-  container: {
-    position: "relative",
-    overflow: "hidden", // Ensure video doesn't overflow the container
-  },
-  video: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    objectFit: "cover",
-  },
-  videoOverlay: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    minWidth: "100%",
-    height: "100vh",
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    pointerEvents: "none",
-  },
   sponsorUs: {
     backgroundColor: "#150D0C",
     padding: "6rem",
