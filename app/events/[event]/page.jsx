03814b5c1d092b377ae02dbec010ffc02d738b6e
Vansh Mittal <mittalvansh69@gmail.com>
2023-10-03 00:01:47 +0530

    Made Event Details Section Static!

--- app/events/[event]/page.jsx
+++ app/events/[event]/page.jsx
@@ -1,11 +1,11 @@
-"use client";
 import { Box } from "@mantine/core";
 import MainAppShell from "@/app/components/MainAppShell";
 import HeroSection from "@/app/components/EventDetailsSection/HeroSection";
 import EventDetails from "@/app/components/EventDetailsSection/EventDetails";
 import EventGuidelines from "@/app/components/EventDetailsSection/EventGuidelines";
+import { getAllEvents } from "@/app/utils/apis";
 
-export default function Events() {
+export default function Events({ event }) {
   return (
     <MainAppShell>
       <Box style={{ backgroundColor: "#0F0F0F" }}>
@@ -15,4 +15,16 @@ export default function Events() {
       </Box>
     </MainAppShell>
   );
+}
+
+export async function getStaticPaths() {
+  const eventDetails = await getAllEvents();
+  const events = eventDetails.data.events;
+
+  return {
+    paths: events.map((event) => ({
+      params: { event: event.slug },
+    })),
+    fallback: false,
+  };
 }
--- app/utils/apis.js
+++ app/utils/apis.js
@@ -1,6 +1,6 @@
 import axios from "axios";
+
 export const login = (email, password) => {
-  console.log({ email, password });
   return axios.post(
     "https://tarang-backend.onrender.com/api/login",
     {
@@ -29,4 +29,9 @@ export const getUser = () => {
   return axios.get("https://tarang-backend.onrender.com/api/user", {
     withCredentials: true,
   });
+};
+
+/* Events */
+export const getAllEvents = () => {
+  return axios.get("https://tarang-backend.onrender.com/api/events");
 };
