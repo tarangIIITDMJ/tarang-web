2aefbdebdf3a8d4a0d670a4990f413d7e54d617d
Nishanth019 <bhukyanishanth69@gmail.com>
2023-10-08 23:23:37 +0530

    Modified 404 page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,40 +1,47 @@
 "use client";
 import {Text, Flex, Box , Button} from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
-import { IconArrowLeft } from "@tabler/icons-react";
+import NotFoundImage from "@/public/NotFoundImage.svg";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NotFound() {
   const isMobileView = useMediaQuery("(max-width: 768px)");
 
   return (
     <Box style={styles.container}>
-      <Flex gap={isMobileView ? '2rem' : '6rem'} 
+      <Flex  
        align={'center'}
-       direction={isMobileView ? 'column' : 'row'} 
+       direction={'column'} 
        >
-        <Box 
-         w={isMobileView ? "15rem" : "30rem"}
-         h={isMobileView ? "15rem" : "30rem"}
-         bg={"#CCCCCC"}></Box>
-        <Flex  direction={'column'}    align={isMobileView ? 'center' : 'flex-start'}>
-          <Text fw={"500"} ta={isMobileView?"center":""} size={isMobileView ? '4rem' : '6rem'} pb={"1rem"}>
-            404
+           <Box
+          w={isMobileView ? "20rem" : "55rem"}
+          h={isMobileView ? "15rem" : "30rem"}
+          mb={isMobileView ? "0rem" : "-5rem"}
+          style={{ position: "relative" }}
+        >
+          <Image
+            src={NotFoundImage}
+            alt="Not Found"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+        </Box>
+        <Flex  direction={'column'}    align={'center'}>
+          <Text lh={isMobileView?'2rem':'2.5rem'} fw={"500"} color={'#FFFFFF'} ta={"center"} size={isMobileView ? '1.5rem' : '2rem'} pb={isMobileView?"1rem":"2rem"}>
+            Oops, We can't seem <br/> to find the page you are looking for.
           </Text>
-          <Text fw={"600"} ta={isMobileView ? "center" : ""}  size="md" pb={"3.5rem"}>
-            Oops! Looks like you've taken a wrong turn. <br/>
-            The page you're looking for couldn't be found.  <br/> Let's get you back on track.
+          <Text fw={"600"} color={'#B4B4B4'} ta={isMobileView ? "center" : ""}  size="md" pb={isMobileView?"3rem":"3.5rem"}>
+            The page you are looking for might have been removed, had its name changed, or is temporarily unavailable
           </Text>
           <Link href="/">
             <Button
               bg="#ffc40c"
               c="black"
               radius={0}
-              size={"lg"}
+              size={isMobileView?"md":"lg"}
               fw={"600"}
-              leftSection={<IconArrowLeft />}
             >
-               Go to homepage
+               Back to homepage
             </Button>
           </Link>
         </Flex>
@@ -49,7 +56,7 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'white', 
+    backgroundColor: '#2B2B2B', 
   },
 };
 
