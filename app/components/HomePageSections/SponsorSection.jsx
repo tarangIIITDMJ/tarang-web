be9634fa1a618e9bdd7449e08791e7e178c37a2d
Prajjwal kapoor <68912239+prajjwalkapoor@users.noreply.github.com>
2023-10-31 16:33:24 +0530

    feat: sponsor bar added (#70)

--- app/components/HomePageSections/SponsorSection.jsx
+++ app/components/HomePageSections/SponsorSection.jsx
@@ -1,40 +1,41 @@
 "use client";
 
-import { Box, Stack, Text, Group } from "@mantine/core";
-import { useMediaQuery } from "@mantine/hooks";
-
+import { Box, Image, Text } from "@mantine/core";
+import cssstyles from "@/app/styles/home.module.css";
 export default function SponsorSection() {
-  const isMobileView = useMediaQuery("(max-width: 768px)");
+  const sponsors = [
+    "/sponsors/hp.png",
+    "/sponsors/asanzo.png",
+    "/sponsors/bostin-cafe.png",
+    "/sponsors/hatchers.png",
+    "/sponsors/indianbank.png",
+    "/sponsors/kathination.png",
+    "/sponsors/kitchenopolis.png",
+    "/sponsors/lofa.png",
+    "/sponsors/lord-of-the-drinks.png",
+    "/sponsors/mocha.png",
+    "/sponsors/redfm.png",
+    "/sponsors/sardar-g-malai-chaap.png",
+    "/sponsors/titan.png",
+  ];
 
   return (
-    <Box
-      style={styles.sponsorUs}
-      px={isMobileView ? "1rem" : "6.75rem"}
-      py={isMobileView ? "4rem" : "6rem"}
-    >
-      <Stack align="center" gap="2rem" w={"100%"}>
-        <Text size={isMobileView ? "20px" : "48px"} c="#F2F2F2">
-          Our sponsors
+    <>
+      <Box bg={"#1E1E1E"} pt="4rem" pb="2rem">
+        <Text ta="center" c="#fff" size={"3rem"} fw={500}>
+          Our Sponsors
         </Text>
-        <Group gap="1.5rem" justify="center" w={isMobileView ? "18rem" : null}>
-          {Array(6)
-            .fill()
-            .map((_, index) => (
-              <Box
-                key={index}
-                w={isMobileView ? "6.25rem" : "10.5rem"}
-                h={isMobileView ? "6.25rem" : "6.5rem"}
-                bg="#3D3D3D"
-              ></Box>
+        <div className={cssstyles.marquee}>
+          <Box className={cssstyles.marqueeContent} py={50}>
+            {sponsors.map((sponsor, i) => (
+              <Image key={i} src={sponsor} alt="jupyter" w={100} />
             ))}
-        </Group>
-      </Stack>
-    </Box>
+            {sponsors.slice(0, 6).map((sponsor, i) => (
+              <Image key={i} src={sponsor} alt="jupyter" w={100} />
+            ))}
+          </Box>
+        </div>
+      </Box>
+    </>
   );
 }
-
-const styles = {
-  sponsorUs: {
-    backgroundColor: "#150D0C",
-  },
-};
